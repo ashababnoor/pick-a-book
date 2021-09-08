80078d6ecebd3ed231418b2af1f98cc97dbfca6e
Shabab <ashababnoor@gmail.com>
2021-09-08 12:38:02 +0600

    added templates

---
+++ pick_a_book/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('', views.home, name='home'),
+]
--- pick_a_book/main/views.py
+++ pick_a_book/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    context = {}
+
+    return render(request, 'main/home.html', context)
+
+
+def about(request):
+    context = {}
+
+    return render(request, 'main/about.html', context)

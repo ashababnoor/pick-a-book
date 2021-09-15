f34a5e4123ad8af65361d626e53151cc0687c91d
Shabab <ashababnoor@gmail.com>
2021-09-15 16:47:25 +0600

    updated posts and main

--- pick_a_book/posts/urls.py
+++ pick_a_book/posts/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 app_name = 'posts'
 
-urlpatterns = [] 
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+] 

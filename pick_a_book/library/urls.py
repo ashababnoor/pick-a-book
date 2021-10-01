81a179997b42af44afe3901a1a946d36c4c283c7
Shabab <ashababnoor@gmail.com>
2021-10-01 10:46:52 +0600

    updated library layout and urls

--- pick_a_book/library/urls.py
+++ pick_a_book/library/urls.py
@@ -6,6 +6,6 @@ app_name='library'
 urlpatterns = [
     path("search/", views.lsblank, name="LSblank"),
     path("location_processing/", views.location_processing, name="location_processing" ),
-    path("search/libid<int:id>",views.libraryDetails,name="library_details")
+    path("search/libid/<int:id>",views.libraryDetails,name="library_details")
     # path("process_loc/", views.process_loc, name="process_loc" )
 ]
